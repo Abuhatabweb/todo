a2aa2a9b6cfc4122e2b54bf145ceb045afc39f2c
Abuhatabweb <165955515+Abuhatabweb@users.noreply.github.com>
2024-04-04 14:45:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,32 @@ let input = document.querySelector(".text-box")
 let btn = document.querySelector(".submit")
 let taskDiv = document.querySelector(".task-div")
 
-taskArray = []
-// return from local storage
-localstorageReturn = function() {
-    if(taskArray != ""){
-    tasks =window.localStorage.getItem("tasks")
-    taskArray = JSON.parse(tasks)
-    taskArray.forEach(task => {
+let taskArray = []
+
+getFromStorage()
+
+// Add task
+btn.onclick = function () {
+    if(input.value != "") {
+        addTaskToArray(input.value);
+        input.value = ''
+    }
+}
+
+function addTaskToArray(inputText){
+    let task = {
+        id: Date.now(),
+        text: inputText,
+        complete: false
+    }
+    taskArray.push(task)
+    addElementToPage(taskArray)
+    addElementToStorage(taskArray)
+}
+
+function addElementToPage(taskArray) {
+    taskDiv.innerHTML = ""
+    taskArray.forEach((task) => {
         let div = document.createElement("div")
         div.className = "task"
         div.setAttribute("id" , task.id)
@@ -18,52 +37,47 @@ localstorageReturn = function() {
         del.innerHTML = "Delete"
         div.appendChild(del)
         taskDiv.appendChild(div)
+        if(task.complete == true){
+            div.classList.add("active")
+        }
     });
-} }
-localstorageReturn()
-
-btn.onclick = function () {
-    let task = {
-        id: Date.now(),
-        text: input.value,
-        done: false
-    } 
-    // Add task to page
-    if(task.text !=""){
-    input.value = ""
-    let div = document.createElement("div")
-    div.className = "task"
-    div.setAttribute("id" , task.id)
-    div.innerHTML= task.text
-    let del = document.createElement("span")
-    del.className = "del"
-    del.innerHTML = "Delete"
-    div.appendChild(del)
-    taskDiv.appendChild(div)
-    // Add task to array
-    addToArray(task)
-    // Add array to local storage
-    arrayToLocal(taskArray)
-}}
-
-
-addToArray = function (task) {
-    taskArray.push(task)
 }
 
-arrayToLocal = function(array){
+function addElementToStorage(taskArray){
     window.localStorage.setItem("tasks" , JSON.stringify(taskArray))
+}
+
+function getFromStorage () {
+  let data =  window.localStorage.getItem("tasks")
+  if (data){
+    let tasks = JSON.parse(data)
+    taskArray = tasks
+    addElementToPage(taskArray)
+  }
 }
 
 // remove task 
 taskDiv.addEventListener("click" , (e) => {
+    //delete
     if(e.target.classList.contains("del")){
         e.target.parentElement.remove();
         // remove from storage
         taskArray = taskArray.filter((task) => task.id != e.target.parentElement.getAttribute("id"))
-        arrayToLocal(taskArray)
+        addElementToStorage(taskArray)
+    }
 
+    // complete
+    if(e.target.classList.contains("task")){
+       e.target.classList.toggle("active")
+       completeUpdate(e.target.getAttribute("id"))
     }
 })
 
-
+function completeUpdate(taskId){
+    for (let i = 0; i < taskArray.length; i++) {
+        if (taskArray[i].id == taskId) {
+            taskArray[i].complete == false ? (taskArray[i].complete = true) : (taskArray[i].complete = false);
+        } 
+    }
+    addElementToStorage(taskArray)
+}
